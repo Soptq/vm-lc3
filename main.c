c6d9af09a525d9dde7d4e1a5bbd17fbf422f5c38
Soptq <32592090+soptq@users.noreply.github.com>
2020-07-17 13:45:56 +0800

    - implementations completed

--- main.c
+++ main.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <signal.h>
+/* unix */
+#include <unistd.h>
+
+#include <sys/time.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 /* 10 registers: R0-R7, PC, COND */
 enum {
@@ -58,6 +66,12 @@ enum {
     TRAP_HALT = 0x25    /* halt the program */
 };
 
+/* 2 memory mapped registers */
+enum {
+    MR_KBSR = 0xFE00,   /* keyboard status */
+    MR_KBDR = 0xFE02    /* keyboard data */
+};
+
 /* 2^16 memory locations */
 u_int16_t memory[UINT16_MAX];
 
@@ -66,6 +80,8 @@ uint16_t reg[R_TOTAL];
 
 /* flag of whether the vm is running */
 int running = 1;
+
+struct termios original_tio;
 
 /* extend sign number. filling in 0's for positive numbers and 1's for negative numbers. */
 uint16_t sign_extend(uint16_t x, int bit_count){
@@ -86,15 +102,83 @@ void update_flags(uint16_t r){
     }
 }
 
+uint16_t check_key(){
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+void disable_input_buffering(){
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+void handle_interrupt(int signal) {
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
+/* swap the lower 8 bits and higher 8 bits */
+uint16_t swap16(uint16_t x) {
+    return (x << 8) | (x >> 8);
+}
+
+/* read instructions from a file */
+void read_image_file(FILE* file){
+    /* read the origin of the code */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    /* read the rest of the file */
+    uint16_t max_read = UINT16_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    while (read-- > 0) {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+/* read instructions from a path leading to the file */
+int read_image(const char* image_path){
+    FILE* file = fopen(image_path, "rb");
+    if (!file) {return 0;}
+    read_image_file(file);
+    fclose(file);
+
+    return 1;
+}
+
 /* read instructions from PC */
 uint16_t mem_read(uint16_t address) {
-    uint16_t test;
-    return test;
+    if (address == MR_KBSR) {   /* read keyboard status*/
+        if (check_key()) {
+            memory[MR_KBSR] = (1 << 15);    /* flags of start reading from keyboard */
+            memory[MR_KBDR] = getchar();    /* keyboard signal will be stored here */
+        } else {
+            memory[MR_KBSR] = 0;    /* reset keyboard status */
+        }
+    }
+    return memory[address];
 }
 
 /* write instructions from PC */
 void mem_write(uint16_t address, uint16_t value) {
-
+    memory[address] = value;
 }
 
 /*
@@ -120,18 +204,6 @@ void instr_add(uint16_t instr) {
 }
 
 /*
- * LDI R0 PCoffset; load the memory address of (PC + PCoffset) to the R2
- * bits:   15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
- *          1  0  1  0    DR       PCoffset9
- * */
-void instr_ldi(uint16_t instr){
-    uint16_t r0 = (instr >> 9) * 0x7;
-    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
-    reg[r0] = mem_read(mem_read(reg[R_PC + pc_offset]));
-    update_flags(r0);
-}
-
-/*
  * AND R0, R1, R2;
  * bits:   15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
  * mode 1:  0  0  0  1    DR    SR1  0 0 0  SR2
@@ -153,6 +225,18 @@ void instr_and(uint16_t instr){
 }
 
 /*
+ * NOT DR, SR;
+ * bits:   15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
+ *          1  0  0  1    DR     SR  1 1 1 1 1 1
+ * */
+void instr_not(uint16_t instr){
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t r1 = (instr >> 6) & 0x7;
+    reg[r0] = ~reg[r1];
+    update_flags(r0);
+}
+
+/*
  * BR LABEL
  * bits:   15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
  *         0  0  0  0  n  z       PCoffset9
@@ -163,6 +247,18 @@ void instr_branch(uint16_t instr){
     if (br_flag & reg[R_COND]) {
         reg[R_PC] += pc_offset;
     }
+}
+
+/*
+ * LDI R0 PCoffset; load the memory address of (PC + PCoffset) to the R2
+ * bits:   15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
+ *          1  0  1  0    DR       PCoffset9
+ * */
+void instr_ldi(uint16_t instr){
+    uint16_t r0 = (instr >> 9) & 0x7;
+    uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+    reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offset));
+    update_flags(r0);
 }
 
 /*
@@ -182,7 +278,7 @@ void instr_jump(uint16_t instr){
  * */
 void instr_jsr(uint16_t instr){
     reg[R_R7] = reg[R_PC];
-    uint16_t jsr_flag = (instr >> 11) * 0x1;
+    uint16_t jsr_flag = (instr >> 11) & 0x1;
     if (jsr_flag) {
         uint16_t pc_offset = sign_extend(instr & 0x7FF, 11);
         reg[R_PC] += pc_offset;
@@ -200,7 +296,7 @@ void instr_jsr(uint16_t instr){
 void instr_ld(uint16_t instr){
     uint16_t r0 = (instr >> 9) & 0x7;
     uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
-    reg[r0] = mem_read(reg[r0] + pc_offset);
+    reg[r0] = mem_read(reg[R_PC] + pc_offset);
     update_flags(r0);
 }
 
@@ -226,18 +322,6 @@ void instr_lea(uint16_t instr){
     uint16_t r0 = (instr >> 9) & 0x7;
     uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
     reg[r0] = reg[R_PC] + pc_offset;
-    update_flags(r0);
-}
-
-/*
- * NOT DR, SR;
- * bits:   15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
- *          1  0  0  1    DR     SR  1 1 1 1 1 1
- * */
-void instr_not(uint16_t instr){
-    uint16_t r0 = (instr >> 9) & 0x7;
-    uint16_t r1 = (instr >> 6) & 0x7;
-    reg[r0] = ~reg[r1];
     update_flags(r0);
 }
 
@@ -342,7 +426,6 @@ void trap_halt(){
  *          1  1  1  1  0  0 0 0   trapvect8
  * */
 void instr_trap(uint16_t instr){
-    reg[R_R7] = reg[R_PC];
     switch (instr & 0xFF) {
         case TRAP_GETC:
             trap_getc();
@@ -363,7 +446,6 @@ void instr_trap(uint16_t instr){
             trap_halt();
             break;
     }
-    reg[R_PC] = mem_read();
 }
 
 
@@ -390,6 +472,9 @@ int main(int argc, const char* argv[]){
         }
     }
 
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
+
     /* default PC start position, 0x3000 */
     enum { PC_START = 0x3000 };
     reg[R_PC] = PC_START;
@@ -406,7 +491,7 @@ int main(int argc, const char* argv[]){
                 instr_add(instr);
                 break;
             case OP_AND:
-                instr_add(instr);
+                instr_and(instr);
                 break;
             case OP_BR:
                 instr_branch(instr);
@@ -451,9 +536,12 @@ int main(int argc, const char* argv[]){
                 instr_trap(instr);
                 break;
             default:
+                abort();
                 break;
         }
     }
+
+    restore_input_buffering();
 
     return 0;
 }
